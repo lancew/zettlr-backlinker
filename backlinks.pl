f388495667545332a6aa9eead6229f7eced1e30c
Lance Wicks <lw@judocoach.com>
2020-08-02 22:37:10 +0100

    Add first pass script

--- backlinks.pl
+++ backlinks.pl
@@ -13,12 +13,20 @@ my $ZB = Zettlr::Backlinker->new;
 my $files = $ZB->get_file_list('/home/lancew/zettelkasten');
 my $backlinks = $ZB->get_backlinks_from_files(@$files);
 
-#warn Dumper $backlinks;
+warn Dumper $backlinks;
 
 
-my @no_links;
+
+
 for my $file (keys %$backlinks) {
-    warn $file if @{$backlinks->{$file}} > 0;
+    if (@{$backlinks->{$file}} > 0){
+
+        $ZB->insert_backlinks(
+            $file,
+            @{$backlinks->{$file}}
+        );
+
+    }
 
 }
 
